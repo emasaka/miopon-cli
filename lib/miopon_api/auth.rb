3fd4aa977f581c75f64209f78264d9cf7472f457
emasaka <emasaka@gmail.com>
2014-01-04 02:48:59 +0900

    MioponAPI::Auth class
    execute PhantomJS to call OAuth 2.0 Implicit Grant

---
+++ lib/miopon_api/auth.rb
@@ -0,0 +1,47 @@
+require 'pathname'
+require 'cgi'
+require 'open3'
+require 'json'
+
+class MioponAPI
+  class Auth
+    def initialize(params)
+      @params = params
+    end
+
+    JSFILE = Pathname(__FILE__).realpath.dirname.
+      join('auth_by_phantomjs.js').to_s
+
+    def by_phantomjs(user, password, moreparams = {})
+      allparams = @params.merge(moreparams)
+      url = gen_url(allparams)
+      # Don't give passwords as command argument!
+      rtn = Open3.popen2e({'MIOPON_USER' => user,'MIOPON_PASSWORD' => password},
+                          'phantomjs', JSFILE, url ) do |i, o, t|
+        i.close
+        o.read
+      end
+      store_tokens(JSON.parse(rtn), allparams)
+    end
+
+    def gen_url(params)
+      'https://api.iijmio.jp/mobile/d/v1/authorization/?' +
+        'response_type=token' +
+        "&client_id=#{params[:dev_id]}" +
+        "&redirect_uri=#{CGI.escape(params[:redirect_uri])}" +
+        "&state=#{CGI.escape(params[:state])}"
+    end
+
+    def store_tokens(data, params)
+      # TODO: make some exception class
+      data['token_type'] == 'Bearer' or raise
+      data['state'] == params[:state] or raise
+
+      @access_token = data['access_token']
+      @state = data['state']
+      @expires_in = data['expires_in']
+    end
+
+    attr_reader :access_token, :state, :expires_in
+  end
+end
